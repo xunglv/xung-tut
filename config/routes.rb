d712c016a51338826df2c4dec4b411b5f97f3a5e
Le Van Xung <xunglv@ubuntu.ubuntu-domain>
2010-04-12 08:23:13 +0700

    finish chapter 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
+  
+
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about   '/about',   :controller => 'pages', :action => 'about'
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+
+
+  map.home '/', :controller => 'pages', :action => 'home'
+
+  
+#  puts "\nroute draw called: "+ (defined?(map.home)).to_s
+
+  # You can have the root of your site routed with map.root
+  # -- just remember to delete public/index.html.
+  map.root :controller => 'pages', :action => 'home'
+
+
+
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
@@ -38,6 +56,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
