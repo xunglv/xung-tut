e24510a822ffa1dfafb1c0af9a452ba6763841fc
Le Van Xung <xunglv@gmail.com>
2011-03-28 22:53:29 +0700

    added words to database

--- app/controllers/dicts_controller.rb
+++
@@ -1,2 +0,0 @@
-class DictsController < ApplicationController
-end
--- app/controllers/giapha_controller.rb
+++ app/controllers/giapha_controller.rb
@@ -2,4 +2,8 @@ class GiaphaController < ApplicationController
 	def index
 		render :file => Rails.root.join('public', 'giapha.xhtml'), :layout => false
 	end
+	
+	def lookup
+		
+	end
 end
